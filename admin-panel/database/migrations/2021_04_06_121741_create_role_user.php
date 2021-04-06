b3db51ace788a7fed33957315db56ff720bf7971
trantuyet <trantuyet.fightforfreedom@gmail.com>
2021-04-06 19:39:27 +0700

    button edit / delete hasn't done

--- admin-panel/database/migrations/2021_04_06_121741_create_role_user.php
+++ admin-panel/database/migrations/2021_04_06_121741_create_role_user.php
@@ -14,7 +14,7 @@ class CreateRoleUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('role')
+            $table->integer('role');
         });
     }
 
